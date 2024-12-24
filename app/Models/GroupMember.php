1baad56634115e29b0c23037388480df902bcd95
makiomar <mo7amed.maki@gmail.com>
2024-12-24 12:24:00 +0200

    init

---
+++ app/Models/GroupMember.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use app\User;
+use app\Models\CourseGroup;
+
+class GroupMember extends Model
+{
+    protected $fillable = [
+        'group_id',
+        'student_id',
+    ];
+
+    public function group()
+    {
+        return $this->belongsTo(CourseGroup::class, 'group_id');
+    }
+
+    public function student()
+    {
+        return $this->belongsTo(User::class, 'student_id');
+    }
+}
