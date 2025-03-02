3f05a38c5b03fd266d668d5f2c4b9cfea4201ee3
makiomar <mo7amed.maki@gmail.com>
2025-03-02 11:28:28 +0200

    Hide author name

--- resources/views/web/default/blog/grid-list.blade.php
+++ resources/views/web/default/blog/grid-list.blade.php
@@ -22,13 +22,14 @@
         <div class="mt-20 blog-grid-desc">{!! truncate(strip_tags($post->description), 160) !!}</div>
 
         <div class="blog-grid-footer d-flex align-items-center justify-content-between mt-15">
+            {{--
             <span>
                 <i data-feather="user" width="20" height="20" class=""></i>
                  @if(!empty($post->author->full_name))
                 <span class="ml-5">{{ $post->author->full_name }}</span>
                  @endif
               </span>
-
+            --}}
             <span class="d-flex align-items-center">
                 <i data-feather="message-square" width="20" height="20" class=""></i>
                 <span class="ml-5">{{ $post->comments_count }}</span>
