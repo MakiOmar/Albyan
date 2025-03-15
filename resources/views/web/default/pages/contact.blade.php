e0ec720e91540031f3fe204fbcb1afe7beecfdda
makiomar <mo7amed.maki@gmail.com>
2025-03-15 19:19:25 +0200

    HTML

--- resources/views/web/default/pages/contact.blade.php
+++ resources/views/web/default/pages/contact.blade.php
@@ -165,7 +165,7 @@
                         </div>
         
                         <div class="row">
-                            <div class="col-12 col-md-6">
+                            <div class="col-12">
                                 @include('web.default.includes.captcha_input')
                             </div>
                         </div>
