fc21ee5cbf5fc21413034680feb01e5cb0ab85d9
makiomar <mo7amed.maki@gmail.com>
2025-04-06 09:11:50 +0200

    Define a method to get webinar groups by webinar id

--- app/Http/Controllers/Web/LearningPageController.php
+++ app/Http/Controllers/Web/LearningPageController.php
@@ -12,6 +12,8 @@ use App\Models\Certificate;
 use App\Models\CourseLearningLastView;
 use App\Models\CourseNoticeboard;
 use Illuminate\Http\Request;
+use App\Models\GroupMember;
+use App\Models\CourseGroup;
 
 class LearningPageController extends Controller
 {
@@ -85,7 +87,55 @@ class LearningPageController extends Controller
             ->where('user_id', $user->id)
             ->where('webinar_id', $course->id)
             ->first();
-
+        $groups = $this->getMyGroupsForWebinar($course->id);
+        $data['groups'] = $groups;
         return view('web.default.course.learningPage.index', $data);
+    }
+    public function getMyGroupsForWebinar($webinarId)
+    {
+        $userId = auth()->id();
+
+        $groups = CourseGroup::where('webinar_id', $webinarId)
+            ->whereHas('members', function ($query) use ($userId) {
+                $query->where('student_id', $userId);
+            })
+            ->get();
+
+        return $groups;
+    }
+    public function groupNextTime($courseGroup, &$joinUrl, &$meetingID, $role = 'teacher')
+    {
+        $joinURL = $role === 'teacher' ? 'start_url' : 'join_url';
+        $nextStartTime = false;
+
+        if ($courseGroup->meeting_json) {
+            $decodedJson = json_decode($courseGroup->meeting_json, true);
+            if ($decodedJson && isset($decodedJson['occurrences'])) {
+                $joinUrl     = $decodedJson[$joinURL];
+                $occurrences = $decodedJson['occurrences'];
+                $meetingID   = $decodedJson['id'];
+
+                // Get user's timezone and set default if not valid
+                $userTimezone = auth()->user()->timezone ?? '';
+                if ($userTimezone !== 'Asia/Dubai' && $userTimezone !== 'Africa/Cairo') {
+                    $userTimezone = 'Asia/Dubai';
+                }
+
+                // Get current datetime in the user's (or default) timezone
+                $currentDateTime = Carbon::now($userTimezone);
+
+                // Filter occurrences to find the next closest session
+                $nextSession = collect($occurrences)->filter(
+                    function ($occurrence) use ($currentDateTime, $userTimezone) {
+                        return Carbon::parse($occurrence['start_time'])->setTimezone($userTimezone)->greaterThan($currentDateTime);
+                    }
+                )->sortBy('start_time')->first(); // Sort by start_time and get the first one
+                if ($nextSession) {
+                    $nextStartTime = Carbon::parse($nextSession['start_time'])->setTimezone($userTimezone)->toIso8601String();
+                }
+            }
+        }
+
+        return $nextStartTime;
     }
 }
