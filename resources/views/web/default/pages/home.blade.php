808d00c6564aabe725f32ca8474c7fcc0ec481a1
makiomar <mo7amed.maki@gmail.com>
2025-06-02 18:02:17 +0300

    Trendy categories new design

--- resources/views/web/default/pages/home.blade.php
+++ resources/views/web/default/pages/home.blade.php
@@ -322,40 +322,11 @@
                 </div>
             </section>
         @endif
-        @if($homeSection->name == \App\Models\HomeSection::$trend_categories and !empty($trendCategories) and !$trendCategories->isEmpty())
-            <section class="home-sections home-sections-swiper container">
-                <h2 class="section-title">{{ trans('home.trending_categories') }}</h2>
-                <p class="section-hint">{{ trans('home.trending_categories_hint') }}</p>
-
-
-                <div class="swiper-container trend-categories-swiper px-12 mt-40">
-                    <div class="swiper-wrapper py-20">
-                        @foreach($trendCategories as $trend)
-                            <div class="swiper-slide">
-                                <a href="{{ $trend->category->getUrl() }}">
-                                    <div class="trending-card d-flex flex-column align-items-center w-100">
-                                        <div class="trending-image d-flex align-items-center justify-content-center w-100" style="background-color: {{ $trend->color }}">
-                                            <div class="icon mb-3">
-                                                <img src="{{ $trend->getIcon() }}" width="10" class="img-cover" alt="{{ $trend->category->title }}">
-                                            </div>
-                                        </div>
-
-                                        <div class="item-count px-10 px-lg-20 py-5 py-lg-10">{{ $trend->category->webinars_count }} {{ trans('product.course') }}</div>
-
-                                        <h3>{{ $trend->category->title }}</h3>
-                                    </div>
-                                </a>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
-
-                <div class="d-flex justify-content-center">
-                    <div class="swiper-pagination trend-categories-swiper-pagination"></div>
-                </div>
-            </section>
-        @endif
         --}}
+        @if($homeSection->name == \App\Models\HomeSection::$trend_categories and !empty($trendCategories) and !$trendCategories->isEmpty())
+            @include('web.default.pages.includes.categories-rounded')
+        @endif
+        
         {{-- Ads Bannaer --}}
         @if($homeSection->name == \App\Models\HomeSection::$full_advertising_banner and !empty($advertisingBanners1) and count($advertisingBanners1))
             <div class="home-sections container">
---
+++ resources/views/web/default/pages/includes/categories-rounded.blade.php
@@ -0,0 +1,58 @@
+<section class="home-sections">
+    
+    <img src="/store/1/Group1321314336.png" style="position: absolute;z-index: -1;left: 0;top: -50px;height: 170px;">  
+        <div class="container text-center">
+            <h2 class="section-title mb-4" style="min-width: 200px;position: relative; display: inline-block;background-image: url('store/1/Union.png');background-position: center;background-repeat: no-repeat;background-size: contain">
+
+            <span style="color: white; padding: 8px 24px; border-radius: 0 20px 0 20px;">
+                الأقسام
+            </span>
+        </h2>
+
+        <div class="g-3 row">
+            
+            {{-- زر كل الأقسام --}}
+            <div class="col-md-3 p-2">
+                <a href="#" class="btn btn-primary rounded-pill px-4 py-2 d-flex align-items-center gap-2 position-relative position-relative">
+                    <i class="fas fa-th-large"></i>
+                    كل الأقسام
+                    <span class="position-absolute" style="right: -20px;top: -40px;">
+                    <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_142_528)">
+                        <path d="M38.1359 28.9227C38.669 27.4683 38.8692 25.9128 38.7217 24.3708C38.5741 22.8288 38.0825 21.3396 37.2832 20.0127C36.4839 18.6858 35.3971 17.5551 34.1029 16.7039C32.8087 15.8526 31.3401 15.3025 29.8052 15.094C28.2702 14.8854 26.7081 15.0239 25.2337 15.499C23.7593 15.9742 22.4103 16.774 21.286 17.8396C20.1616 18.9051 19.2906 20.2093 18.7371 21.656C18.1835 23.1028 17.9615 24.6553 18.0873 26.1992L19.0131 38.2539L27.2351 41.2715L35.7391 32.6775C36.8019 31.6153 37.6198 30.3338 38.1359 28.9227Z" fill="#FFC43D"/>
+                        <path d="M31.9809 15.6303C31.5447 15.4739 31.0986 15.3469 30.6455 15.2502C32.0793 16.0063 33.3154 17.0886 34.2544 18.4099C35.1935 19.7311 35.8091 21.2544 36.0517 22.8571C36.2944 24.4598 36.1572 26.0971 35.6514 27.6371C35.1456 29.1771 34.2851 30.5768 33.1394 31.7234L24.6354 40.3173L27.2351 41.2714L35.7391 32.6775C36.9679 31.4471 37.8672 29.9272 38.3542 28.2578C38.8412 26.5885 38.9003 24.8234 38.5259 23.1253C38.1515 21.4271 37.3558 19.8505 36.212 18.5407C35.0682 17.2309 33.6131 16.23 31.9809 15.6303Z" fill="#FAAE17" fill-opacity="0.7"/>
+                        <path d="M34.6763 8.28595L33.5048 11.4782" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M44.8008 17.7539L41.7271 19.2073" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M45.0284 31.5338L41.837 30.3625" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M20.8308 8.95667L22.2346 12.0534" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M11.7432 19.3179L14.9347 20.4892" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M38.1359 28.9227C38.669 27.4683 38.8692 25.9128 38.7217 24.3708C38.5741 22.8288 38.0825 21.3396 37.2832 20.0127C36.4839 18.6858 35.3971 17.5551 34.1029 16.7039C32.8087 15.8526 31.3401 15.3025 29.8052 15.094C28.2702 14.8854 26.7081 15.0239 25.2337 15.499C23.7593 15.9742 22.4103 16.774 21.286 17.8396C20.1616 18.9051 19.2906 20.2093 18.7371 21.656C18.1835 23.1028 17.9615 24.6553 18.0873 26.1992L19.0131 38.2539L27.2351 41.2715L35.7391 32.6775C36.8019 31.6153 37.6198 30.3338 38.1359 28.9227Z" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M19.0067 38.2516L27.2416 41.2738L26.1337 44.2925C25.7329 45.3845 24.9147 46.2726 23.8592 46.7614C22.8036 47.2502 21.5971 47.2996 20.5051 46.8988C19.4131 46.498 18.525 45.6799 18.0362 44.6243C17.5474 43.5687 17.498 42.3622 17.8988 41.2702L19.0067 38.2516Z" fill="#727373" fill-opacity="0.7"/>
+                        <path d="M24.6395 40.3188L23.5316 43.3375C23.2139 44.2006 22.6328 44.942 21.8706 45.4568C21.1084 45.9716 20.2037 46.2337 19.2844 46.2062C19.6517 46.5006 20.0642 46.7337 20.506 46.8964C21.0467 47.0948 21.6212 47.1848 22.1967 47.1613C22.7722 47.1377 23.3374 47.001 23.8601 46.7589C24.3827 46.5169 24.8526 46.1743 25.2429 45.7507C25.6331 45.3271 25.9361 44.8308 26.1346 44.2901L27.2416 41.2738L24.6395 40.3188Z" fill="#606161" fill-opacity="0.7"/>
+                        <path d="M19.0067 38.2516L27.2416 41.2738L26.1337 44.2925C25.7329 45.3845 24.9147 46.2726 23.8592 46.7614C22.8036 47.2502 21.5971 47.2996 20.5051 46.8988C19.4131 46.498 18.525 45.6799 18.0362 44.6243C17.5474 43.5687 17.498 42.3622 17.8988 41.2702L19.0067 38.2516Z" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M16.9406 40.7335L27.2116 44.503" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M17.9886 37.8779L28.2596 41.6475" stroke="black" stroke-opacity="0.7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </g>
+                        <defs>
+                        <clipPath id="clip0_142_528">
+                        <rect width="43" height="43" fill="white" transform="translate(14.8151) rotate(20.1535)"/>
+                        </clipPath>
+                        </defs>
+                    </svg>
+                    </span>
+
+                </a>
+            </div>
+            @foreach($trendCategories as $trend)
+                <div class="col-md-3 p-2">
+                    <a href="{{ $trend->category->getUrl() }}" class="btn trendy-category btn-outline text-dark rounded-pill px-4 py-2 d-flex align-items-center" style="border: 1px solid #000">
+                        <img src="{{ $trend->getIcon() }}" class="ml-1" style="max-height: 20px" alt="{{ $trend->category->title }}">
+                        {{ $trend->category->title }}
+                    </a>
+                </div>
+            @endforeach
+            
+        </div>
+    </div>  
+    <img src="/store/1/Ellipse.png" style="position: absolute;z-index: -1;right: 0;bottom: -50px;height: 170px;"> 
+</section>
